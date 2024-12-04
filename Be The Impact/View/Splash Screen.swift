f36afdf90a1ad21a5223086657451ba442e95595
max <maldosari24@twq.idserve.net>
2024-12-04 15:14:39 +0300

    mvvm folders

---
+++ Be The Impact/View/Splash Screen.swift
@@ -0,0 +1,17 @@
+//
+//  Be_The_ImpactApp.swift
+//  Be The Impact
+//
+//  Created by Mashael Aldosari on 03/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Be_The_ImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
