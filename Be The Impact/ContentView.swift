1b63596bbd1b8064abc81e0bdfe8b29d4dafeaad
max <maldosari24@twq.idserve.net>
2024-12-04 13:17:07 +0300

    Initial Commit

---
+++ Be The Impact/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Be The Impact
+//
+//  Created by Mashael Aldosari on 03/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Be The ImpactTests/Be_The_ImpactTests.swift
@@ -0,0 +1,17 @@
+//
+//  Be_The_ImpactTests.swift
+//  Be The ImpactTests
+//
+//  Created by Mashael Aldosari on 03/06/1446 AH.
+//
+
+import Testing
+@testable import Be_The_Impact
+
+struct Be_The_ImpactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
